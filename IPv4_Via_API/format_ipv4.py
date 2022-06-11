0a1b8198214ee02d239cc602682cb382b822109b
DawoodShah009 <dawood2019@namal.edu.pk>
2022-06-11 16:59:19 +0500

    Added IP_trimmed_v01/v02 successfully

--- IPv4_Via_API/format_ipv4.py
+++ IPv4_Via_API/format_ipv4.py
@@ -2,95 +2,85 @@ import binascii
 from hashlib import new
 import itertools 
 import socket
-
-def upgrade_ip(ip, subnet, lower_range, upper_range, byte_to_change, bits_to_change):
+def upgrade_ip(ip, subnet, lower_range, upper_range, byte_to_change, bits_to_change, ip_list):
     ip_temp = ip.split('.')
     byte = ip_temp[byte_to_change]
-    ip_list = []                    
 
-    if subnet > lower_range and subnet < upper_range:                               # upgrate subnet.
+    if subnet > lower_range and subnet < upper_range:                               
         difference = subnet - lower_range
         binary = bin(int(byte))
 
-        binary = binary[2:len(ip)]   # remove xb
-        if len(binary) < 8:                                                         # make binary len == to 8
+        binary = binary[2:len(ip)]   
+        
+        if len(binary) < 8:                                                         
             while len(binary) != 8:
                 binary = str(0)+binary
 
         network_portion = binary[0:(8-bits_to_change)]
-        n = 8 - difference
-        lst = list(itertools.product(['0', '1'], repeat=n))      # combinations of ip
-    
-        for index in lst:
 
+        n = 8 - difference
+        lst = list(itertools.product(['0', '1'], repeat=n))   
+        for index in lst[1:]:
+            
             value = ''.join(index)
             new_byte = int(network_portion+value,2)
-
             ip_temp[byte_to_change] = str(new_byte)
             ip_address = '.'.join(ip_temp)
             ip_list.append(ip_address)
 
     return ip_list
         
-def split_IP_and_covert_to_Hex(ipv4):
+def split_IP_and_covert_to_Hex(ipv4, ip_list):
+
     ipv4 = ipv4.split('/')
     ip = ipv4[0]
+    ret_ip = ip
     subnet = ipv4[1]
-    hex_ip_list = []
-    # subnet= subnet[ : len(subnet)-2]
-    # print(subnet, "hla")
     subnet = int(subnet)
     
     if subnet < 8:
         bits_to_change = 8-subnet
-        ip_list = upgrade_ip(ip, subnet, 0, 8, 0, bits_to_change)
-        subnet = str(8)
-
+        ip_list = upgrade_ip(ip, subnet, 0, 8, 0, bits_to_change, ip_list)
+        
     elif subnet > 8 and subnet < 16:   # upgrate subnet.
         bits_to_change = 16-subnet
-        
-        ip_list = upgrade_ip(ip, subnet, 8, 16, 1, bits_to_change)
-        subnet = str(16)
+        ip_list = upgrade_ip(ip, subnet, 8, 16, 1, bits_to_change, ip_list)
 
     elif subnet > 16 and subnet < 24:
         bits_to_change = 24-subnet
-        
-        ip_list = upgrade_ip(ip, subnet, 16, 24, 2, bits_to_change)            #104.101.220.0/23
-        subnet = str(24)
-
-    elif subnet ==8 or subnet == 16 or subnet >= 24:
-        ip_list = [ip]
+        ip_list = upgrade_ip(ip, subnet, 16, 24, 2, bits_to_change, ip_list)            #104.101.220.0/23
 
     subnet = str(subnet)
-    for ip in ip_list:
-        ip = str(binascii.hexlify(socket.inet_aton(ip)).upper())
-        ip = ip[2:len(ip)-2]
-        hex_ip = "0x"+ip
-        hex_ip_list.append(hex_ip)
+    ip = str(binascii.hexlify(socket.inet_aton(ret_ip)).upper())
+    ip = ip[2:len(ip)-2]
+    hex_ip = "0x"+ip
+    return hex_ip , ip_list, subnet, ret_ip
+        
     
-    return hex_ip_list , ip_list, subnet
 
 list_of_files = ["akamai","amazon_cloudfront","aryaka","cachefly","cdn77","cdnetworks","cloudfare","fastly",
                  "incapsula","limelight01","limelight02","rackspace01","rackspace02","softlayer","stackpath"]
-
+val_list = [8,16,24]
 length = len(list_of_files)
 files_index = 0
+check_list = []    
 while files_index < length:
+    ip_list = []
+    final_list = []
     file = list_of_files[files_index]
     ip_file = open(file+".txt", "r")
     var = file.upper()
-    print("/*"+var+"*/")
+    if files_index == (length-1):
+        print("/*CDN_"+var+"*/")
+    else:
+        print("/*APPLICATION_"+var+"*/")
+        
     
-    for index, ipv4  in enumerate(ip_file):      
-        hex_ip_list , ipv4_list, subnet = split_IP_and_covert_to_Hex(ipv4)
-
-        for i in range(len(hex_ip_list)):           
-            print(" ","{",hex_ip_list[i],"/*",ipv4_list[i],"*/,",subnet+",",var,"},")       
+    for index, ipv4  in enumerate(ip_file):   
+  
+        hex_ip , ip_list, subnet, ip = split_IP_and_covert_to_Hex(ipv4, ip_list)
+        if (ip not in final_list) and (ip not in ip_list):
+            print(" ","{",hex_ip,"/*",ip,"*/,",subnet+",",var,"},") 
+            final_list.append(ip)
+                  
     files_index+=1 
-
-
-    
-    
-
-
-
